a7316c3f8ddd0cfafc56d9342c22aa06a9015025
syungkeaw <syungkeaw@gmail.com>
2015-07-24 22:19:01 +0700

    fix search not found

--- views/movie/search.php
+++ views/movie/search.php
@@ -3,15 +3,20 @@ use yii\helpers\Html;
 ?>
 <div class="row">
 	<div class="col-md-8">
-		<?php foreach($model as $lv1){ ?>
-			<?php foreach($lv1 as $lv2){ ?>
-				<div class="list-group">
-				 	<?= Html::a('
-						<h4 class="list-group-item-heading">'.$lv2->title.'</h4>
-				   	 	<p class="list-group-item-text">'.strip_tags($lv2->title_description).'</p>
-				 	', ['index', 'id' => $lv2->id], ['class' => 'list-group-item']) ?>
-				</div>	
+		<?php if(is_object($model)){ ?>
+			<?php foreach($model as $lv1){ ?>
+				<?php foreach($lv1 as $lv2){ ?>
+					<div class="list-group">
+					 	<?= Html::a('
+							<h4 class="list-group-item-heading">'.$lv2->title.'</h4>
+					   	 	<p class="list-group-item-text">'.strip_tags($lv2->title_description).'</p>
+					 	', ['index', 'id' => $lv2->id], ['class' => 'list-group-item']) ?>
+					</div>	
+				<?php } ?>
 			<?php } ?>
+		<?php }else{ ?>
+			<p>เนื่องจากมีผู้ใช้งานระบบมากเกินไป กรุณาลองใหม่อีกครั้ง</p>
+
 		<?php } ?>
 	</div>
 	<div class="col-md-4">
